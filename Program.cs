c129b5279ca2319b68cdfb2da3fe4f9b10ff4219
tysonyu87 <taiyou940304@gmail.com>
2022-02-17 22:34:04 +0800

    0

--- Program.cs
+++ Program.cs
@@ -11,4 +11,5 @@ System.Console.WriteLine(name.Substring(1,1)); // 分割字串 (位置,往後幾
 System.Console.WriteLine(System.Math.Abs(-565)); // 取絕對值
 System.Console.WriteLine(System.Math.Pow(3,2)); // 次方(底數,次數)
 System.Console.WriteLine(System.Math.Sqrt(8)); // 開根號
-System.Console.WriteLine(System.Math.Max(2,78)); // 兩數比大小
+System.Console.WriteLine(System.Math.Max(2,78)); 
+System.Console.WriteLine(System.Math.Min(2,78)); // 兩數比大小
